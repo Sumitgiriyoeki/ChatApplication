660a9ec8ee0d7a4f449eb352d3362f426eba2cc9
Sumitgiriyoeki <sumit.g@yoekisoft.in>
2026-05-01 10:40:39 +0530

    deployed file

--- frontened/chatbot/next.config.ts
+++ frontened/chatbot/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: "export", 
+  images: {
+    unoptimized: true, // required for static export
+  },
+   basePath: process.env.NODE_ENV === "production"
+    // ? "/frontened/chatbot"
+     ? "/ChatApplication" : "",
+
+   assetPrefix: process.env.NODE_ENV === "production"
+    ? "/ChatApplication/" : "",
 };
 
 export default nextConfig;
