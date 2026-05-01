bf79772aea55ca2dc825b06262e168076ae67d98
Sumitgiriyoeki <sumit.g@yoekisoft.in>
2026-05-01 10:37:38 +0530

    Updates

--- _next/static/kbYkxGSan3Bv4PQ7LTKP2/_buildManifest.js
+++ _next/static/kbYkxGSan3Bv4PQ7LTKP2/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ChatApplication//_next/:path+",
+        "destination": "/ChatApplication/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
